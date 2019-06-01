53de2b0e903251c0394fc89ec866ea74ec37035c
Antonio Barbosa <avildes@gmail.com>
2019-06-01 18:07:38 -0300

    add String constructor where if else was used to read values

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ void connect()
     Serial.println("Waiting to connect...");
 
     //TODO trocar o while por um for
-    //TODO se não conseguir conectar reiniciar o nodemcu
+    //TODO se nao conseguir conectar reiniciar o nodemcu
   }
 
   Serial.print("IP address: ");
@@ -80,37 +80,27 @@ void updatePins()
   {
     return;
   }
-  //JsonObject &root = jsonBuffer.parseObject(payload);
 
-  int D0 = doc["D0"]; // 1
-  int D1 = doc["D1"]; // 1
-  int D2 = doc["D2"]; // 1
-  int D3 = doc["D3"]; // 1
-  int D4 = doc["D4"]; // 1
-  int D5 = doc["D5"]; // 1
-  int D6 = doc["D6"]; // 1
-  int D7 = doc["D7"]; // 1
-  int D8 = doc["D8"]; // 1
-  int RX = doc["RX"]; // 1
-  int TX = doc["TX"]; // 1
-  int S2 = doc["S2"]; // 1
-  int S3 = doc["S3"]; // 1
+  int D0 = doc["D0"];
+  int D1 = doc["D1"];
+  int D2 = doc["D2"];
+  int D3 = doc["D3"];
+  int D4 = doc["D4"];
+  int D5 = doc["D5"];
+  int D6 = doc["D6"];
+  int D7 = doc["D7"];
+  int D8 = doc["D8"];
+  int RX = doc["RX"];
+  int TX = doc["TX"];
+  int S2 = doc["S2"];
+  int S3 = doc["S3"];
 
   D0Value = D0;
 
   digitalWrite(D0Pin, D0Value);
   //digitalWrite(5, D1);
 
-  if(D0Value == 1)
-  {
-    server.send(200, "text/plain", "1 Ok");
-  }
-  else
-  {
-    server.send(200, "text/plain", "0 Ok");
-
-  }
-
+  server.send(200, "text/plain", String(D0Value));
   Serial.println("POST Ok");
 }
 
@@ -122,13 +112,6 @@ void handleNotFound()
 
 void handleRoot()
 {
-  String message = "0";
-
-  if(D0Value == 1)
-  {
-    message = "1";
-  }
-
-  server.send(200, "text/plain", message);
+  server.send(200, "text/plain", String(D0Value));
   Serial.println("GET Ok");
 } 
