976df067e8965f06b42009c6ae8d9a6e1a43d35f
Antonio Barbosa <avildes@gmail.com>
2019-06-02 19:25:00 -0300

    add pin mode configuration

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,35 @@ const char *password = "52337260";
 int D0Pin = 16;
 int D0Value;
 
+enum pMode
+{
+  mINPUT = 0,
+  mOUTPUT = 1,
+  mINPUT_PULLUP = 2
+};
+
+uint8_t convertPinMode(pMode mode)
+{
+    uint8_t response;
+
+    switch (mode)
+    {
+        case mINPUT:
+          response = INPUT;
+          break;
+        case mOUTPUT:
+          response = OUTPUT;
+          break;
+        case mINPUT_PULLUP:
+          response = INPUT_PULLUP;
+          break;
+        
+        default:
+          response = INPUT;
+    }
+    return response;
+}
+
 void updatePins()
 {
   if (server.hasArg("plain") == false)
@@ -21,34 +50,55 @@ void updatePins()
 
   String json = server.arg("plain");
 
-  const size_t capacity = JSON_OBJECT_SIZE(13) + 80;
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 80;
   DynamicJsonDocument doc(capacity);
 
   DeserializationError error = deserializeJson(doc, json);
 
-  if(error)
+  if (error)
   {
     return;
   }
 
   int D0 = doc["D0"];
-  int D1 = doc["D1"];
-  int D2 = doc["D2"];
-  int D3 = doc["D3"];
-  int D4 = doc["D4"];
-  int D5 = doc["D5"];
-  int D6 = doc["D6"];
-  int D7 = doc["D7"];
-  int D8 = doc["D8"];
-  int RX = doc["RX"];
-  int TX = doc["TX"];
-  int S2 = doc["S2"];
-  int S3 = doc["S3"];
 
   D0Value = D0;
 
   digitalWrite(D0Pin, D0Value);
   //digitalWrite(5, D1);
+
+  server.send(200, "text/plain", String(D0Value));
+  Serial.println("POST Ok");
+}
+
+void updatePinMode()
+{
+  //TODO remover parte inicial esperando como resposta um DynamicJsonDocument
+  //TODO remover parte final e colocar uma resposta padrão para todos ou uma resposta única desse método
+
+  if (server.hasArg("plain") == false)
+  { //Check if body received
+    Serial.println("plain false");
+    server.send(200, "text/plain", "Body not received");
+    return;
+  }
+
+  String json = server.arg("plain");
+
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 80;
+  DynamicJsonDocument doc(capacity);
+
+  DeserializationError error = deserializeJson(doc, json);
+
+  if (error)
+  {
+    return;
+  }
+
+  int mode = doc["pinMode"];
+  int pin = doc["pin"];
+
+  digitalWrite(pin, convertPinMode(static_cast<pMode>(mode)));
 
   server.send(200, "text/plain", String(D0Value));
   Serial.println("POST Ok");
@@ -62,21 +112,21 @@ void handleNotFound()
 
 void handleRoot()
 {
-    String message = "{\n\"D0\": " + String(digitalRead(D0)) + ",\n" +
-                      "\"D1\": " + String(digitalRead(D1)) + ",\n" +
-                      "\"D2\": " + String(digitalRead(D2)) + ",\n" +
-                      "\"D3\": " + String(digitalRead(D3)) + ",\n" +
-                      "\"D4\": " + String(digitalRead(D4)) + ",\n" +
-                      "\"D5\": " + String(digitalRead(D5)) + ",\n" +
-                      "\"D6\": " + String(digitalRead(D6)) + ",\n" +
-                      "\"D7\": " + String(digitalRead(D7)) + ",\n" +
-                      "\"D8\": " + String(digitalRead(D8)) + ",\n" +
-                      "\"A0\": " + String(analogRead(A0)) + "\n" +
-                     "}";
+  String message = "{\n\"D0\": " + String(digitalRead(D0)) + ",\n" +
+                   "\"D1\": " + String(digitalRead(D1)) + ",\n" +
+                   "\"D2\": " + String(digitalRead(D2)) + ",\n" +
+                   "\"D3\": " + String(digitalRead(D3)) + ",\n" +
+                   "\"D4\": " + String(digitalRead(D4)) + ",\n" +
+                   "\"D5\": " + String(digitalRead(D5)) + ",\n" +
+                   "\"D6\": " + String(digitalRead(D6)) + ",\n" +
+                   "\"D7\": " + String(digitalRead(D7)) + ",\n" +
+                   "\"D8\": " + String(digitalRead(D8)) + ",\n" +
+                   "\"A0\": " + String(analogRead(A0)) + "\n" +
+                   "}";
 
-    server.send(200, "application/json", message);
-    Serial.println("GET Ok");
-} 
+  server.send(200, "application/json", message);
+  Serial.println("GET Ok");
+}
 
 void connect()
 {
